f689dad6a2f6f998ffb55bedf7fce0ee84d4babc
Antoine Febrier <antoine.febrier@etu.minesparis.psl.eu>
2025-07-03 11:17:29 +0200

    maj

--- osman.py
+++ osman.py
@@ -1,11 +1,8 @@
 import pandas as pd
 import re
 
-<<<<<<< HEAD
-=======
 chemin_fichier = "Gjoa MdP/Test/20250515-Devis-client-projet.xlsx"
 
->>>>>>> ba97a27df7e062726302d369e5ad9d8effd191f4
 def df_to_string(dataframe):
     """Transforme un DataFrame en chaîne avec les règles demandées, en nettoyant les doublons."""
     lignes = []
@@ -45,7 +42,6 @@ def extraire_tous_les_lots_depuis_excel(fichier_excel, nom_feuille='Devis'):
 
 # Exemple d'utilisation
 if __name__ == "__main__":
-    chemin_fichier = "Gjoa MdP/Exemple 2/20250401-Devis-Client-Exemple 2.xlsx"
     resultats = extraire_tous_les_lots_depuis_excel(chemin_fichier)
 
     for cle, valeur in resultats.items():
